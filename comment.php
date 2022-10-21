5e3d07fcd01c263f992a2f94476a6032a5b728ce
nurfadeelamama <s64309010011@ptnicec.ac.th>
2022-10-21 11:53:42 +0700

    ส่งไฟล์ Comment ครั้งที่2

---
+++ comment.php
@@ -0,0 +1,15 @@
+<?php
+
+//บรรทัดที่ 6-7 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+echo "Nurfadeela mama <br>";
+echo "Imformation Technology <br> ";
+//echo "Pattani <br>";
+#echo "Thailand <br>";
+
+//บรรทัดที่ 10-12 แสดงผลการใช้งาน Comment แบบหลายบรรทัด
+/*echo "<br>"
+echo "94000 <br>";
+echo "Asia <br>";*/
+echo "Universal ";
+
+?>
