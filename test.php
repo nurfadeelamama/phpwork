781d1150a0d313b5b14c2dceb24ef25b0d85168a
nurfadeelamama <s64309010011@ptnicec.ac.th>
2022-10-21 11:29:17 +0700

    ส่งไฟล์ ครั้งที่1

--- test.php
+++ test.php
@@ -1,6 +1,26 @@
 <?php
 
-echo "Nurfadeela Mama";
-
-
+  echo "Nurfadeela Mama";
+  
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TEST</title>
+</head>
+<body>
+  <br> Information Technology </br>
+  
+  <?php
+    echo "Pattani"
+  ?>
+  <br>
+</body>
+</html>
+ <?php
+    echo "Thailand"
+  ?>
